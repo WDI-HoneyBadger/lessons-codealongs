da0d15c507faf99de0665220f29126af96426f0d
trevorpreston <trevorpreston4@gmail.com>
2018-11-20 16:41:46 +0300

    add event listeners and forms

--- w02d03-JS-DOM/main.js
+++ w02d03-JS-DOM/main.js
@@ -23,17 +23,17 @@ var nav = document.querySelector('.nav');
 var container1 = document.querySelector('.container');
 var firstProf = document.querySelector('#first-profile');
 
-console.log(nav);
-console.log(container1)
+// console.log(nav);
+// console.log(container1)
 
 /* querySelectorAll */
 var allContainers = document.querySelectorAll('.container');
 var alATags = document.querySelectorAll('a');
 var navElements = document.querySelectorAll('.nav > div');
-console.log(allContainers);
+// console.log(allContainers);
 
 var header = document.querySelector('h1');
-console.log(header);
+// console.log(header);
 header.style.color = 'darkgreen';
 
 function randomBg() {
@@ -53,4 +53,55 @@ image.className = 'smaller-img';
 
 var body = document.getElementsByTagName('body')[0];
 
-container1.innerHTML = '<h1>OH NO</h1> <p>I just changed the whole page</p>';
+// container1.innerHTML = '<h1>OH NO</h1> <p>I just changed the whole page</p>';
+
+var project4Container = document.querySelector('.project4');
+
+var newImg = document.createElement('img');
+newImg.src = 'https://placekitten.com/300/700';
+newImg.style.border = '2px solid black';
+
+project4Container.appendChild(newImg);
+
+var newParagraph = document.createElement('p');
+var paragraphText = document.createTextNode('screeeeeee');
+
+newParagraph.appendChild(paragraphText);
+project4Container.appendChild(newParagraph);
+
+var navOption1 = document.querySelectorAll('.nav > div')[0];
+
+// navOption1.addEventListener('click', function() {
+//   console.log('click');
+// })
+
+function changeBodyColor() {
+  console.log('CLICK!!!!');
+  body.style.backgroundColor = 'cyan';
+}
+
+navOption1.addEventListener('click', changeBodyColor);
+
+var input = document.querySelector('#user-word');
+var button = document.querySelector('#user-button');
+var resultSection = document.querySelector('.form-result');
+
+function capitalizeAndAppend(){
+  console.log('clickity click');
+  // get the value of the input
+  var userInput = input.value;
+  console.log('input value is ', userInput);
+  // capitalize that value
+  var capitalizedInput = userInput.toUpperCase();
+  console.log(capitalizedInput);
+  // append the capitalized value to the resultSection
+  var newPTag = document.createElement('p');
+  var newPTagText = document.createTextNode(capitalizedInput);
+
+  newPTag.appendChild(newPTagText);
+  resultSection.appendChild(newPTag);
+}
+
+button.addEventListener('click', capitalizeAndAppend);
+
+
