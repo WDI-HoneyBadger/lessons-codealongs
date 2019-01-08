b10c56c903928fdf4e94247ae860baa7bc9bcc54
trevorpreston <trevorpreston4@gmail.com>
2019-01-08 14:11:32 +0300

    add scaffolded components

---
+++ w08d03-fetch-in-react/advice-app/src/components/RandomQuote.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class RandomQuote extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+  render(){
+    return(
+      <div>
+        RandomQuote placeholder
+      </div>
+    )
+  }
+}
+
+export default RandomQuote;
---
+++ w08d03-fetch-in-react/advice-app/src/components/Search.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+  render(){
+    return(
+      <div>
+        Search placeholder
+      </div>
+    )
+  }
+}
+
+export default Search;
